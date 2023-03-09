44b42405a1affb8ef829c79a0bf7f9439dfc0a10
the3plet <soorajp2002@gmail.com>
2023-03-09 19:51:42 +0530

    Basic blogs data and state added

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
 
 const Home = () => {
-
+  const[blogs,setBlogs]= useState([
+    {title:'How to learn reactjs',body:'lorem ipsum...', author:'Camio',id:1},
+    {title:'React full roadmap',body:'lorem ipsum...', author:'Rijoy',id:2},
+    {title:'Best tutorial for learning react',body:'lorem ipsum...', author:'Bijoy',id:3},
+  ]);
   return (
     <div className="home">
-      <h3>Home Page</h3>
+      {blogs.map((blog) =>(
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+          </div>
+      ))}
     </div>
   );
 };
