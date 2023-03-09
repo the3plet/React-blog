6a61e98fb8b17ae0ecdf4220a0a7f3123242882a
the3plet <soorajp2002@gmail.com>
2023-03-09 23:02:26 +0530

    added blog delete functionality

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,17 @@ const Home = () => {
   const[blogs,setBlogs]= useState([
     {title:'How to learn reactjs',body:'lorem ipsum...', author:'Camio',id:1},
     {title:'React full roadmap',body:'lorem ipsum...', author:'Rijoy',id:2},
-    {title:'Best tutorial for learning react',body:'lorem ipsum...', author:'Bijoy',id:3},
+    {title:'Best tutorial for learning react',body:'lorem ipsum...', author:'Camio',id:3},
   ]);
+
+
+  const handleDelete=(id)=>{
+  const newBlogs= blogs.filter(blog => blog.id !== id)
+  setBlogs(newBlogs)
+  }
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All My Blogs"}/>
+      <BlogList blogs={blogs} title={"All My Blogs"} handleDelete={handleDelete}/>
     </div>
   );
 };
